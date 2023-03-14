017a3bd8319a33c4c63fe8b69c89f34b5ae1aee7
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-14 15:45:20 +0900

    객체 메서드 실행

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,8 +1,20 @@
 package kr.hs.study;
 
+import kr.hs.study.beans.HelloworldEn;
+import kr.hs.study.beans.HelloworldKr;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Calendar;
+
 public class Main {
     public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
 
+        HelloworldKr kr = ctx.getBean("kr", HelloworldKr.class);
+        kr.hello();
+
+        HelloworldEn en = ctx.getBean("en", HelloworldEn.class);
+        en.hello();
 
         System.out.println("Hello world!");
     }
--- src/main/java/kr/hs/study/beans/HelloworldEn.java
+++ src/main/java/kr/hs/study/beans/HelloworldEn.java
@@ -2,6 +2,6 @@ package kr.hs.study.beans;
 
 public class HelloworldEn implements Hello{
     public void hello() {
-        System.out.println("안녕하세요");
+        System.out.println("hi");
     }
 }
