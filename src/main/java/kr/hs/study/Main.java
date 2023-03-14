98790cbe70cbbf35c905399548502f3aa0f7ad6c
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-14 15:41:13 +0900

    Initial commit

---
+++ src/main/java/kr/hs/study/Main.java
@@ -0,0 +1,9 @@
+package kr.hs.study;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/kr/hs/study/beans/HelloworldKr.java
@@ -0,0 +1,7 @@
+package kr.hs.study.beans;
+
+public class HelloworldKr implements Hello{
+    public void hello() {
+        System.out.println("안녕하세요");
+    }
+}
